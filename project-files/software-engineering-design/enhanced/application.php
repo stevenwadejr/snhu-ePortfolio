01a7e05a77984f4d0d1ef421db6f65ce49043bb2
Steven Wade <stevenwadejr@gmail.com>
2021-05-23 11:42:32 -0400

    Add error handling when running the application

--- project-files/software-engineering-design/enhanced/application.php
+++ project-files/software-engineering-design/enhanced/application.php
@@ -12,9 +12,14 @@ use stevenwadejr\hash\HashTableCommand;
 use stevenwadejr\linked\LinkedListCommand;
 use stevenwadejr\sorting\SortingCommand;
 
-$application = new Application(__DIR__);
-$application->add(new BinarySearchTreeCommand('run-binary'));
-$application->add(new HashTableCommand('run-hash'));
-$application->add(new LinkedListCommand('run-linked'));
-$application->add(new SortingCommand('run-sort'));
-$application->run();
+try {
+    $application = new Application(__DIR__);
+    $application->add(new BinarySearchTreeCommand('run-binary'));
+    $application->add(new HashTableCommand('run-hash'));
+    $application->add(new LinkedListCommand('run-linked'));
+    $application->add(new SortingCommand('run-sort'));
+    $application->run();
+} catch (Exception $e) {
+    echo 'An error occurred while trying to run the application: ' . $e->getMessage();
+    exit(1);
+}
